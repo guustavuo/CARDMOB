3c262726dc3bcc73497eda99004f0e7280df1f24
Gustavo Paixão <gustavo.nardi@aluno.ifsp.edu.br>
2025-05-13 09:14:49 -0300

    TextInput basic examples

--- Etapa.2/app-react-exemplos/App.js
+++ Etapa.2/app-react-exemplos/App.js
@@ -1,18 +1,14 @@
 import React, { useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
+import { Text, View, Button, Image, TextInput, StyleSheet } from 'react-native';
 
-import List from './components/List';
+import Inputs from './components/Inputs';
 
 export default function App() {
  
   return (
     <View style={styles.container}>
-      <List />
-      <View style={styles.redbox}/>
-      <View style={styles.bluebox}/>
-      <View style={styles.blackbox}/>
-      
+      <Inputs />      
     </View>
   );
 }
@@ -26,23 +22,5 @@ const styles = StyleSheet.create({
     height: 600,
     marginTop: 150,
 
-  },
-  redbox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'red',
-    borderRadius: 50,
-  },
-  bluebox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'blue',
-    borderRadius: 50,
-  },
-  blackbox: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'black',
-    borderRadius: 50,
   }
 });
---
+++ Etapa.2/app-react-exemplos/App2.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button, Image, TextInput, FlatList } from 'react-native';
+
+import List from './components/List';
+
+export default function App() {
+ 
+  return (
+    <View style={styles.container}>
+      <List />
+      <View style={styles.redbox}/>
+      <View style={styles.bluebox}/>
+      <View style={styles.blackbox}/>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    backgroundColor: 'gray',
+    height: 600,
+    marginTop: 150,
+
+  },
+  redbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'red',
+    borderRadius: 50,
+  },
+  bluebox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'blue',
+    borderRadius: 50,
+  },
+  blackbox: {
+    width: 100,
+    height: 100,
+    backgroundColor: 'black',
+    borderRadius: 50,
+  }
+});
