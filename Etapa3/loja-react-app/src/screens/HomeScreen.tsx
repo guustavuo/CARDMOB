e5b65eb137d32ee9baedf1a382e48465076485eb
Gustavo Paixão <gustavo.nardi@aluno.ifsp.edu.br>
2025-08-05 09:05:10 -0300

    Inclui tela HomeScreen

---
+++ Etapa3/loja-react-app/src/screens/HomeScreen.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+import { View, Text } from "react-native";
+
+function HomeScreen () {
+    return (
+        <view>
+            <text>Home Screen</text>
+        </view>
+    )
+}
+
+export default HomeScreen;
